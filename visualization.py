d39b0755b6da6585982bb21da4ebf11a8028b88a
Elizabeth Orji <cegscutie4rill@yahoo.com>
2025-06-18 11:08:37 +0100

    changed .ipynb to .py due to unresolved errors.

---
+++ visualization.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+# Load built-in dataset
+df = sns.load_dataset("tips")
+
+# Line Plot
+plt.plot(df['total_bill'])
+plt.title("Line Plot of Total Bill")
+plt.xlabel("Index")
+plt.ylabel("Total Bill")
+plt.show()
+
+# Scatter Plot
+plt.scatter(df['total_bill'], df['tip'])
+plt.title("Scatter Plot of Total Bill vs Tip")
+plt.xlabel("Total Bill")
+plt.ylabel("Tip")
+plt.show()
+
+# Histogram
+sns.histplot(df['total_bill'], kde=True)
+plt.title("Histogram of Total Bill")
+plt.show()
+
+# Box Plot
+sns.boxplot(x='day', y='total_bill', data=df)
+plt.title("Box Plot of Total Bill by Day")
+plt.show()
